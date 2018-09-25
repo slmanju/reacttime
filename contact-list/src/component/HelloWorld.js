9180dcaf5c1f63e5bf9b2d29fb83420cc598a73f
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-09-25 13:57:22 +0530

    add hello world

---
+++ contact-list/src/component/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default HelloWorld
