a57b1275194d2db3832254051aa88c2e2f026b97
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-09-27 12:33:03 +0530

    add actions

--- users-list/src/components/user-item.js
+++ users-list/src/components/user-item.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 class UserRow extends Component {
 
+  handleClick(event) {
+      console.log('clicked');
+      this.props.onClick();
+  }
+
   render() {
-    return <li>
+    return <li onClick="handleClick">
               { this.props.firstName } { this.props.lastName }
            </li>
   }
--- users-list/src/components/user-list.js
+++ users-list/src/components/user-list.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import UserRow from './user-item';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { selectUser } from '../actions/index';
 
 class UserList extends Component {
+
+  handleClick(user) {
+      this.props.selectUser(user);
+  }
 
   renderList() {
     let users = this.props.users;
     return users.map(user => {
-        return <UserRow firstName={ user.firstName }
-                        lastName={ user.lastName }
-                        key={ user.id } />
+        // return <UserRow firstName={ user.firstName }
+        //                 lastName={ user.lastName }
+        //                 key={ user.id }
+        //                 onClick={ this.props.selectUser } />
+        return <li key={ user.id } onClick={ () => this.handleClick(user) }>
+                  { user.firstName } { user.lastName }
+               </li>
     });
   }
 
@@ -30,4 +41,8 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(UserList)
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({ selectUser }, dispatch);
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(UserList)
