5598a044484a1926a9d98746ddbd88b9428d0eef
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-10-01 08:33:45 +0530

    initial setup

--- issue-tracker/src/App.js
+++ issue-tracker/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import IssueList from './views/tracker/IssueList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <IssueList />
       </div>
     );
   }
---
+++ issue-tracker/src/views/tracker/IssueList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class IssueList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue List</h1>
+        <hr/>
+        <h4>Issue filter</h4>
+        <hr/>
+        <h4>Issue table</h4>
+        <hr/>
+        <h4>Issue add</h4>
+      </div>
+    )
+  }
+}
+
+export default IssueList
